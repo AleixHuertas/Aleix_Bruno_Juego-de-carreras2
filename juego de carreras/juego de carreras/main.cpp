f47935af5103abc72b7242a9daa2c1c37e63c63d
bruno-rmf <bruno.salvatella@estudiant.enti.cat>
2025-12-01 10:26:30 +0100

    kebap pati

--- juego de carreras/juego de carreras/main.cpp
+++ juego de carreras/juego de carreras/main.cpp
@@ -1,40 +1,66 @@
 #include <iostream>
+#include <windows.h>
+
 #define CASILLAS 50
 bool gameOver = false;
 char player1 = 'X';
 char player2 = 'O';
 int playerTurn = 1;
+int tablero[CASILLAS];
+int objetos;
+int dado;
 
 void tiroDado() {
 	srand(time(0));
-	int dado = rand() % 6 + 1;
+	dado = rand() % 6 + 1;
 }
 
+void iniciartablero() {
+	for (int i = 1; i <= CASILLAS; i++)
+	{
+		tablero[i] = i;
+	}
+}
 void tableroimprimir() {
 	for (int i = 1; i <= CASILLAS; i++)
 	{
-		std::cout << i << " ";
-		if (i == 20 || i == 37)
+		if (tablero[i] == 55)
 		{
-			std::cout << '\n';
+			std::cout << 'X' << ' ';
 		}
+		else {
+			std::cout << tablero[i] << ' ';
+		}
+		
+		
 	}
 }
 
+void movimientoPlayer1() {
+	tablero[dado] = 55; // x
+}
+
 int main() {
-	char tablero[CASILLAS];
-	int objetos;
+	
 	tiroDado();
+	iniciartablero();
 	tableroimprimir();
 	while (!gameOver)
 	{
+		system("cls");
+
 		if (playerTurn == 1)
 		{
-
+			tiroDado();
+			movimientoPlayer1();
 		}
 		else if (playerTurn == 2)
 		{
-
+			tiroDado();
+			
 		}
+		tableroimprimir();
+		Sleep(2000);
+
 	}
 }
